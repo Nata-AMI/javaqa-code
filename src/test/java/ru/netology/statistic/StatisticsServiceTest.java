fcbf42ea6521ec6d0bdbaaa0d8d4f293eff137f6
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-03-25 13:36:25 +0500

    BRANCH again3

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,20 +1,13 @@
 package ru.netology.statistic;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 class StatisticsServiceTest {
-
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-        long expected = 12;
-
+        long[] incomesInBillions = {12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 13 };
+        long expected = 13;
         long actual = service.findMax(incomesInBillions);
-
         assertEquals(expected, actual);
     }
 }
